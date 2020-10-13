9ab7567d9ede43fda9434fb742ab397f52e10915
Daniel Ferreira Natal <71156976+dani-fn@users.noreply.github.com>
2020-10-13 09:40:39 -0300

    Add files via upload

---
+++ projetinhos/ex107_exercitando_modulos/moeda.py
@@ -0,0 +1,14 @@
+def metade(v):
+    return v / 2
+
+
+def dobro(v):
+    return v * 2
+
+
+def aumentar(v, percent):
+    return v + (v * percent/100)
+
+
+def diminuir(v, percent):
+    return v * (1 - percent * 0.01)
