fb397e30999a68526edf9ea21a7ee55d5df02e86
Daniel Ferreira Natal <71156976+dani-fn@users.noreply.github.com>
2020-10-13 09:28:10 -0300

    Add files via upload

---
+++ aulas/Aula23_modulos_e_pacotes/__init__.py
@@ -0,0 +1,7 @@
+from Aulas.Aula23_modulos_e_pacotes.uteis import numeros
+
+num = int(input('Digite um número: '))
+fat = numeros.fatorial(num)
+
+print(f'O fatorial de {num} é {fat}')
+print(f'O dobro de {num} é {numeros.dobro(num)}')
