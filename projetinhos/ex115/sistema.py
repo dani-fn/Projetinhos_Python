de09e83733c0569e3f924f82a148f2bf4b807e1f
Daniel Ferreira Natal <71156976+dani-fn@users.noreply.github.com>
2020-10-13 09:53:55 -0300

    Add files via upload

---
+++ projetinhos/ex115/sistema.py
@@ -0,0 +1,22 @@
+from projetinhos.ex115.interface import *  # anotar isto: Para importar tudo
+from projetinhos.ex115.arquivo import *
+from time import sleep as s
+
+cadastros = 'cadastros.txt'
+if not arquivoexiste(cadastros):
+    criararquivo(cadastros)
+while True:
+    opcao = menu(['Ver pessoas cadastradas',
+                  'Cadastrar nova pessoa',
+                  'Sair do Sistema'])
+    s(0.8)
+    if opcao == 1:
+        titulo('PESSOAS CADASTRADAS')
+        lerarquivo(cadastros)
+    if opcao == 2:
+        titulo('NOVO CADASTRO')
+        nome = str(input('Nome: '))
+        idade = leiaint('Idade: ')
+        cadastrar(cadastros, nome, idade)
+    if opcao == 3:
+        exit(titulo('Saindo do sistema... Até logo!'))
